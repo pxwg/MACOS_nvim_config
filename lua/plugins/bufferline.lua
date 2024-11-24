8f9e3721d0b79272a21b678a155e688614cbc50e
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-11-25 03:15:33 +0800

    bufferline disable for copilot-chat

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+  "akinsho/bufferline.nvim",
+  -- highlights = {
+  --   bac = {
+  --     guibg = "#191C28",
+  --   },
+  -- },
+  opts = function(_, opts)
+    opts.options = opts.options or {}
+    opts.options.custom_filter = function(buf_number)
+      local buf_ft = vim.bo[buf_number].filetype
+      if buf_ft == "copilot-chat" or buf_ft == "snacks_dashboard" then
+        return false
+      end
+      return true
+    end
+    opts.options.always_show_bufferline = true
+    return opts
+  end,
+}
