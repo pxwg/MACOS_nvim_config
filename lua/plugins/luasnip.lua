e50d8526d969eaeff3afd0f1157024eb9e74ee84
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-11-24 10:59:59 +0800

    speed up loading

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,7 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    priority = 1000,
-    lazy = false,
+    event = "BufRead",
     config = function()
       require("luasnip").config.set_config({
         enable_autosnippets = true,
