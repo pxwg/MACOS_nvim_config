ac84f6317af7e200730e9f01d61f669149e528c5
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-12-17 19:58:50 +0800

    cusor ai add

--- lua/util/replace.lua
+++ lua/util/replace.lua
@@ -6,13 +6,9 @@ function M.replace_content()
   local cursor_pos = vim.api.nvim_win_get_cursor(current_win)
 
   vim.schedule(function()
-    -- 复制当前选中的内容
     vim.api.nvim_command('normal! "vy')
-    -- 切换到左侧窗口
     vim.api.nvim_command("wincmd h")
-    -- 粘贴内容
     vim.api.nvim_command('normal! gv"vp')
-    -- 切换回原窗口
     vim.api.nvim_set_current_win(current_win)
     vim.api.nvim_win_set_cursor(current_win, cursor_pos)
   end)
