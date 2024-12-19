de1f1cbc03aec5533df508d77f683876ef14acbb
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-12-19 22:02:20 +0800

    integration with sniprun

--- luasnip/tex/mathematica.lua
+++ luasnip/tex/mathematica.lua
@@ -5,11 +5,8 @@ local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
-local f = ls.function_node
 local d = ls.dynamic_node
 local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep
-local line_begin = require("luasnip.extras.expand_conditions").line_begin
 local tex = require("util.latex")
 
 local get_visual = function(args, parent)
@@ -17,34 +14,6 @@ local get_visual = function(args, parent)
     return sn(nil, t(parent.snippet.env.select_raw))
   else -- if select_raw is empty, return a blank insert node
     return sn(nil, i(1))
-  end
-end
-
-local function execute_and_listen(sympy_script)
-  local mma_output = ""
-
-  -- local sympy_script = "Print[hello]"
-  -- local sa = require("sniprun.api")
-  local api_listener_window = function(ind)
-    if ind.status == "ok" then
-      mma_output = ind.message
-    elseif ind.status == "error" then
-      mma_output = ind.message
-    else
-      mma_output = ind.message
-    end
-    mma_output = mma_output:gsub('^"(.*)"$', "%1")
-    return mma_output
-  end
-
-  sa.register_listener(api_listener_window) -- register the listener
-
-  sa.run_string(sympy_script, "mathematica")
-
-  while true do
-    if mma_output ~= sympy_script then
-      return mma_output
-    end
   end
 end
 
@@ -105,15 +74,7 @@ return {
       local job = require("plenary.job")
 
       local sympy_script = string.format(
-        [[
-        a = fullsimplify[toexpression["%s",texform] ];
-        b = texform[a];
-        return["%s =" b]
-        ]],
-        -- origin = re.sub(r'^\s+|\s+$', '', origin)
-        -- parsed = parse_expr(origin)
-        -- output = origin + parsed
-        -- print_latex(parsed)
+        'a = FullSimplify[ToExpression["%s", TeXForm]]; b = TeXForm[a]; Return["%s = " .. ToString[b]]',
         to_eval,
         to_eval
       )
@@ -149,6 +110,7 @@ return {
     d(1, function(_, parent)
       -- gets the part of the block we actually want, and replaces spaces
       -- at the beginning and at the end
+      _G.result = ""
       local to_eval = string.gsub(parent.trigger, "^mcalt(.*)mcalts", "%1")
       -- local new = to_eval
       to_eval = string.gsub(to_eval, "^%s+(.*)%s+$", "%1")
@@ -191,14 +153,27 @@ return {
       local init = _G.result
       local result = init
 
-      sa.run_string(sympy_script, "mathematica")
+      sa.run_string(
+        sympy_script,
+        "mathematica",
+        require("sniprun").setup({
+          display = { "Api" },
+        })
+      )
+
+      sa.run_string(
+        sympy_script,
+        "mathematica",
+        require("sniprun").setup({
+          display = { "Api" },
+        })
+      )
 
       while true do
         if result == init then
           result = _G.result
         elseif result ~= init then
-          _G.result = ""
-          result = result:gsub('^"(.*)"$', "%1")
+          result = result:gsub('[*"]', ""):gsub("\\\\", "\\")
           return sn(nil, { t(result) })
         end
         if os.time() - start_time > timeout then
@@ -206,9 +181,6 @@ return {
         end
         vim.wait(100) -- 添加适当的延迟，避免高 CPU 占用
       end
-      -- if type(result) == "string" then
-      -- end
-      -- return sn(nil, { t(out) })
     end)
   ),
 }
