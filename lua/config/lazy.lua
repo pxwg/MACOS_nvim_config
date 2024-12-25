5fc68121285452ea0162c154f60ef304ee5ada9d
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-12-26 00:05:50 +0800

    switch to mini-diff and only format files which not committed

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,8 @@ vim.o.timeoutlen = 50
 vim.o.ttimeout = true
 vim.o.ttimeoutlen = 10
 -- vim.o.scrolloff = 10
--- vim.g.autoformat = false
+vim.g.autoformat = false
+
 local file_path = "/tmp/nvim_hammerspoon_latex.txt"
 local file = io.open(file_path, "r")
 if not file then
