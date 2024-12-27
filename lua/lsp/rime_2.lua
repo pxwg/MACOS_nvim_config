f97425234ee94fa871b711acbf5e3514e351709c
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-12-28 00:20:06 +0800

    restore rime-ls for neovim v0.10.3

--- lua/lsp/rime_2.lua
+++ lua/lsp/rime_2.lua
@@ -93,7 +93,8 @@ A language server for librime
 
   -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+  -- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+  capabilities.general.positionEncodings = { "utf-8" }
 
   lspconfig.rime_ls.setup({
     init_options = {
