555479ab940556794b0e62d8225f2752a6fe7cd1
向昕宇 <pxwg-dogggie@xiangxinyudeMacBook-Pro.local>
2024-12-28 18:43:01 +0800

    update for copilot

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,3 +1,5 @@
+local replace = require("util.replace_ai")
+local choose = require("util.markdown_code")
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   branch = "canary",
@@ -14,6 +16,45 @@ return {
       complete = {
         detail = "Use @<localleader>s or /<localleader>s for options.",
         insert = "<localleader>s",
+      },
+    },
+    key = {
+      {
+        "n",
+        "v",
+        "<leader>aI",
+        function()
+  choose.select_markdown_code_block()
+  replace.replace_content_and_back()
+        end,
+        { noremap = true, silent = true, desc = "Replace selected text with AI code and back" },
+      },
+      {
+        "n",
+        "v",
+        "<leader>ai",
+        function()
+  choose.select_markdown_code_block()
+  replace.replace_content()
+        end,
+        { noremap = true, silent = true, desc = "Replace selected text with AI code" },
+      },
+      {
+        "n",
+        "<leader>an",
+        function()
+  choose.select_markdown_code_block()
+  replace.insert_content()
+        end,
+        { noremap = true, silent = true, desc = "Insert AI code to the new line" },
+      },
+      {
+        "n",
+        "<leader>ag",
+        function()
+  choose.select_markdown_code_block()
+        end,
+        { noremap = true, silent = true, desc = "Jump and select AI code" },
       },
     },
   },
